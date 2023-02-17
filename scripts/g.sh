86eea31f9166a386e4d13b26f341be6a3ca32ec9
shah zaid rafiuddin <87185156+zaid-coder-01@users.noreply.github.com>
2023-02-17 20:21:39 +0530

    Update g.sh

--- scripts/g.sh
+++ scripts/g.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# navigate to app folder
-cd /home/ubuntu
-
 # install node and npm
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - &&\
 sudo apt-get install -y nodejs
